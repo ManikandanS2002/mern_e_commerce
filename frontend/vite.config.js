6bc792b4f5382224a2387a222d9222ae7ee534b3
ManikandanS2002 <manikandans20052002@gmail.com>
2024-12-09 18:18:32 +0530

    Updated9

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,26 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // This is only used during local development
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Split vendor dependencies into a separate chunk
+          }
+        },
+      },
+    },
+  },  
 });
